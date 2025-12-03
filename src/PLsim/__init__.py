2d3f16509999ea67fdebc0cb7822c6253028dad7
YooJung-Kim <yjkim134340@gmail.com>
2025-12-02 22:03:36 -0800

    undo api

--- src/PLsim/__init__.py
+++ src/PLsim/__init__.py
@@ -1,9 +1,9 @@
-from .simulator import *
-from .lantern import *
-from .scene import *
+# from .simulator import *
+# from .lantern import *
+# from .scene import *
 
-# 2. Expose them in __all__ (Sphinx looks for this!)
-__all__ = []
-__all__.extend(simulator.__all__)
-__all__.extend(lantern.__all__)
-__all__.extend(scene.__all__)
+# # 2. Expose them in __all__ (Sphinx looks for this!)
+# __all__ = []
+# __all__.extend(simulator.__all__)
+# __all__.extend(lantern.__all__)
+# __all__.extend(scene.__all__)
