30305b82b21883928b64e719c19dddc7aaaca8df
YooJung-Kim <yjkim134340@gmail.com>
2026-02-23 18:07:23 -0800

    PIC tutorial

--- src/PLsim/system/lantern.py
+++ src/PLsim/system/lantern.py
@@ -1,4 +1,5 @@
 from scipy.stats import unitary_group
+from scipy.linalg import expm
 import numpy as np
 
 def generate_unitary_matrix(dim, seed=123456):
@@ -24,6 +25,28 @@ def generate_unitary_matrix_near_identity(dim=3, epsilon=0.1):
     A = (A - A.conj().T) / 2
     
     # The matrix exponential of a skew-Hermitian matrix is unitary
-    U = scipy.linalg.expm(A)
+    U = expm(A)
     
+    return U
+
+def make_3x3_unitary(theta_a, theta_b, theta_c = 0):
+    '''
+    Make 3x3 unitary matrix, mixing between LP01 and LP11a modes.
+
+    Parameters
+    theta_a : float
+        Angle for the first mixing term (cos(theta_a) is the amplitude of LP01 mode in port 1)
+    theta_b : float
+        Angle for the second mixing term (phase offset between LP01 and LP11a modes)
+    theta_c : float
+        Angle for the third mixing term (phase offset between two ports. should not do anything)
+    '''
+
+    U = np.identity(3, dtype=complex)
+
+    U[0,0] = np.cos(theta_a)
+    U[0,1] = np.sin(theta_a) * np.exp(1j * theta_b)
+    U[1,0] = np.sin(theta_a) * np.exp(1j * theta_c)
+    U[1,1] = -np.cos(theta_a) * np.exp(1j * (theta_b + theta_c))
+
     return U
